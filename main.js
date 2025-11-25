ac82c844389b32b2f1dbf107534dfeac492a3c90
stevieof <zarichyy@gmail.com>
2025-11-25 04:06:26 +0200

    Add XML output and filtering logic (cylinders, max_mpg)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const { Command } = require("commander");
+const { XMLBuilder } = require("fast-xml-parser");
 
 const program = new Command();
 
@@ -26,25 +27,58 @@ fs.access(INPUT, fs.constants.R_OK, (err) => {
     }
 });
 
-// --- NEW PART FOR COMMIT 4 ---
 async function loadCars() {
     const raw = await fs.promises.readFile(INPUT, "utf-8");
     const lines = raw.split(/\r?\n/).filter(line => line.trim());
     return lines.map(line => JSON.parse(line));
 }
-// --------------------------------
+
+function filterCars(cars, maxMpg) {
+    let result = cars;
+
+    if (!isNaN(maxMpg)) {
+        result = result.filter(car => typeof car.mpg === "number" && car.mpg < maxMpg);
+    }
+
+    return result;
+}
+
+const xmlBuilder = new XMLBuilder({ format: true });
+
+function buildXml(cars, showCylinders) {
+    const xmlCars = cars.map(car => {
+        const obj = {
+            model: car.model,
+            mpg: car.mpg,
+        };
+
+        if (showCylinders) obj.cyl = car.cyl;
+
+        return obj;
+    });
+
+    return xmlBuilder.build({
+        cars: { car: xmlCars }
+    });
+}
 
 function startServer() {
     const server = http.createServer(async (req, res) => {
-        // test JSON reading
+        const url = new URL(req.url, `http://${HOST}:${PORT}`);
+
+        const showCylinders = url.searchParams.get("cylinders") === "true";
+        const maxMpg = url.searchParams.get("max_mpg");
+
         try {
             const cars = await loadCars();
+            const filtered = filterCars(cars, Number(maxMpg));
+            const xml = buildXml(filtered, showCylinders);
 
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ status: "OK", loaded: cars.length }));
+            res.writeHead(200, { "Content-Type": "application/xml" });
+            res.end(xml);
         } catch (e) {
-            res.writeHead(500);
-            res.end("Error loading JSON");
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal Server Error");
         }
     });
 
